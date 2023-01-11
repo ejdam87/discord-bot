0741bf7f80d351932c16148eeed8ccc4882f9996
ejdam87 <adam.dzadon@gmail.com>
2023-01-11 20:54:12 +0100

    Refactor

--- bot.py
+++ bot.py
@@ -1,19 +1,30 @@
+
+import os
+from dotenv import load_dotenv
 import discord
-
-TOKEN = "MTA1MzAwMzEwODUwODI0MTk2MQ.Gg9ICW.WXmlSNCmSNS_dp8w6f-qFfd_8TO-mg_eMULEKg"
-
-client = discord.Client(intents=discord.Intents.default())
-
-@client.event
-async def on_ready():
-    print(f'{client.user.name} has connected to Discord!')
+from discord.ext import commands
 
 
-@client.event
-async def on_message(message):
+HELP_MSG = "To find out when we playing type: '!when'"
+CMD_PREFIX = "!"
 
-    if message.content == '99!':
-        response = random.choice("Hello")
-        await message.channel.send(response)
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
-client.run(TOKEN)
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot( command_prefix=CMD_PREFIX, intents=intents )
+
+@bot.event
+async def on_ready() -> None:
+    print( "Bot has connected to Discord!" )
+
+
+@bot.command( name="when", help=HELP_MSG )
+async def time_table( ctx ) -> None:
+    response = "Halo"
+    await ctx.send( response )
+
+bot.run(TOKEN)
