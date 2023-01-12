705cd0dcc80cf1a8294ab704894b5f8a2da60ef9
ejdam87 <adam.dzadon@gmail.com>
2023-01-12 11:08:01 +0100

    better schedule add

--- bot.py
+++ bot.py
@@ -1,9 +1,17 @@
+
+## --- ENV imports
 import os
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
+## ---
 
-import when
+## --- APPS
+import schedule
+## ---
+
+
+Ctx_type = discord.ext.commands.Context
 
 HELP_MSG = "To find out when we playing type: '!when'"
 CMD_PREFIX = "!"
@@ -23,8 +31,22 @@ async def on_ready() -> None:
 
 
 @bot.command( name="when", help=HELP_MSG )
-async def time_table( ctx ) -> None:
-    response = f"Today we're playing at: {when.get_time_today()}"
+async def time_table( ctx: Ctx_type, *args: str ) -> None:
+
+    if len( args ) == 2 and args[ 0 ]  == "get":
+        day = args[ 1 ]
+        when = schedule.get_playtime( day )
+        response = f"Playtime for {day}: {when}"
+
+    elif len( args ) == 3 and args[ 0 ] == "set":
+        day = args[ 1 ]
+        when = args[ 2 ]
+        schedule.set_playtime( day, when )
+        response = f"Playtime for {day} was set for: {when}"
+
+    else:
+        response = schedule.ERROR_MSG
+
     await ctx.send( response )
 
 bot.run(TOKEN)
---
+++ schedule.py
@@ -0,0 +1,25 @@
+import json
+from datetime import date
+
+PATH = "schedule.json"
+
+ERROR_MSG = "!when [set] <day_name> <time> / !when [get] <day_name>"
+
+
+def get_timetable() -> dict[ str, str ]:
+    with open( PATH, "r" ) as f:
+        return json.load( f )
+
+def store_timetable( timetable: dict[ str, str ] ) -> None:
+    with open( PATH, "w" ) as f:
+        json_str = json.dumps( timetable )
+        f.write( json_str )
+
+def set_playtime( day: str, when: str ) -> None:
+    table = get_timetable( )
+    table[ day ] = when
+    store_timetable( table )
+
+def get_playtime( day: str ) -> str:
+    table = get_timetable( )
+    return table[ day ]
